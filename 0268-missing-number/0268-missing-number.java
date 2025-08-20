7b5847266208db8e87c7953b17c16a5a42f36167
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2025-08-20 18:36:58 +0530

    Time: 0 ms (100%), Space: 45.3 MB (71.52%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,32 +1,50 @@
 class Solution {
-    public int missingNumber(int[] arr) 
+    public int missingNumber(int[] nums) 
     {
-        int n = arr.length;
-        Set<Integer> s = new HashSet<>();
-        
-        for(int a : arr) {
-            s.add(a);
-        }
+        int n = nums.length, sum = 0;
+        int reqSum = n * (n+1)/2;
+        for(int num : nums) sum += num;
 
-        //// Using Bubble Sort
-        // for(int i = 0; i < n; i++) {
-        //     for(int j = 0; j < n - 1; j++) {
-        //         if(arr[j] > arr[j+1]) {
-        //             int temp = arr[j];
-        //             arr[j] = arr[j+1];
-        //             arr[j+1] = temp;
-        //         }
-        //     }
-        // }
-
-        //// Using sort function of Arrays
-        // Arrays.sort(arr);
-
-        for(int i = 0; i < n; i++) {
-            // if(arr[i] != i) return i;
-            if(s.contains(i) == false) return i;
-        }
-
-        return n;
+        return reqSum - sum;
     }
 }
+
+// class Solution {
+//     public int missingNumber(int[] nums) 
+//     {
+//         int n = nums.length;
+//         // Set<Integer> s = new HashSet<>();
+        
+//         // for(int a : arr) {
+//         //     s.add(a);
+//         // }
+
+//         // //// Using Bubble Sort
+//         // // for(int i = 0; i < n; i++) {
+//         // //     for(int j = 0; j < n - 1; j++) {
+//         // //         if(arr[j] > arr[j+1]) {
+//         // //             int temp = arr[j];
+//         // //             arr[j] = arr[j+1];
+//         // //             arr[j+1] = temp;
+//         // //         }
+//         // //     }
+//         // // }
+
+//         // //// Using sort function of Arrays
+//         // // Arrays.sort(arr);
+
+//         // for(int i = 0; i < n; i++) {
+//         //     // if(arr[i] != i) return i;
+//         //     if(s.contains(i) == false) return i;
+//         // }
+
+//         int sum = 0;
+//         int reqSum = n * (n+1)/2;
+//         for(int num : nums) {
+//             sum += num;
+//         }
+
+
+//         return reqSum - sum;
+//     }
+// }
