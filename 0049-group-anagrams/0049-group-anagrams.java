a614fc132ae5745544565075157ac5e449e7644d
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2025-10-26 10:57:39 +0530

    Time: 18 ms (23.8%), Space: 49.1 MB (10.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,23 +1,17 @@
-import java.util.*;
-
 class Solution {
-    static class nvqtiwjzrxka {
-        int[] qydfmsurkpbe;
-        nvqtiwjzrxka(int[] xgzrmopajwq) { this.qydfmsurkpbe = xgzrmopajwq; }
-        @Override public boolean equals(Object hgfytzklqwe) {
-            return hgfytzklqwe instanceof nvqtiwjzrxka && Arrays.equals(qydfmsurkpbe, ((nvqtiwjzrxka) hgfytzklqwe).qydfmsurkpbe);
-        }
-        @Override public int hashCode() { return Arrays.hashCode(qydfmsurkpbe); }
-    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        Map<String, List<String>> map = new HashMap<>();
 
-    public List<List<String>> groupAnagrams(String[] pqslvnmhjkre) {
-        Map<nvqtiwjzrxka, List<String>> mduqpxtlrysa = new HashMap<>();
-        for (String czrhtyplqwas : pqslvnmhjkre) {
-            int[] fklgyxvotqmn = new int[26];
-            for (char qtfznkporysd : czrhtyplqwas.toCharArray()) fklgyxvotqmn[qtfznkporysd - 'a']++;
-            nvqtiwjzrxka udtzyplxqsom = new nvqtiwjzrxka(fklgyxvotqmn);
-            mduqpxtlrysa.computeIfAbsent(udtzyplxqsom, k -> new ArrayList<>()).add(czrhtyplqwas);
+        for(String s : strs){
+            int[] freq = new int[26];
+
+            for(char ch : s.toCharArray()) freq[ch - 'a']++;
+
+            String key = Arrays.toString(freq);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
-        return new ArrayList<>(mduqpxtlrysa.values());
+
+        return new ArrayList<>(map.values());
     }
 }
