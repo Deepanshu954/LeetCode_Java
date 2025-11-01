f1af1f84e25cdcd3118c53629050504adba9ed00
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2025-11-01 10:18:33 +0530

    Time: 0 ms (100%), Space: 43.3 MB (9.12%) - LeetHub

--- 0990-verifying-an-alien-dictionary/0990-verifying-an-alien-dictionary.java
+++ 0990-verifying-an-alien-dictionary/0990-verifying-an-alien-dictionary.java
@@ -1,34 +1,81 @@
+// class Solution {
+//     public boolean isAlienSorted(String[] words, String order) {
+//         Map<Character, Integer> map = new HashMap<>();
+
+//         for(int i = 0; i < 26; i++){
+//             map.put(order.charAt(i), i);
+//         }
+
+        
+
+//         for(int j = 0; j < words.length - 1; j++){
+//             String word1 = words[j];
+//             String word2 = words[j+1];
+
+//             //if(word1.equals("kuvp") && word2.equals("q") return true;
+
+//             if(word1.equals(word2)) continue;
+
+//             int len = Math.min(word1.length(), word2.length());
+//             boolean differenceFound = false;
+
+//             for(int i = 0; i < len; i++){
+//                 char ch1 = word1.charAt(i);
+//                 char ch2 = word2.charAt(i);
+                
+//                 if(ch1 != ch2){
+//                     if(map.get(ch1) > map.get(ch2)) return false;
+//                     differenceFound = true;
+//                     break;
+//                 }
+                
+//             }
+//             if(!differenceFound && word1.length() > word2.length()) return false;
+//         }
+
+//         return true;
+//     }
+// }
+
+
+
+
+
+
 class Solution {
     public boolean isAlienSorted(String[] words, String order) {
-        Map<Character, Integer> map = new HashMap<>();
+        int arr[] = new int[26];
 
-        for (int i = 0; i < 26; i++) {
-            map.put(order.charAt(i), i);
+        for(int i = 0; i < 26; i++){
+            //map.put(order.charAt(i), i);
+            arr[order.charAt(i) - 'a'] = i;
         }
 
-        for (int j = 0; j < words.length - 1; j++) {
-            String word1 = words[j];
-            String word2 = words[j + 1];
+        
 
-            // ❌ This compares references, not content. Must use equals().
-            if (word1.equals(word2)) continue;
+        for(int j = 0; j < words.length - 1; j++){
+            String word1 = words[j];
+            String word2 = words[j+1];
+
+            //if(word1.equals("kuvp") && word2.equals("q") return true;
+
+            if(word1.equals(word2)) continue;
 
             int len = Math.min(word1.length(), word2.length());
             boolean differenceFound = false;
 
-            for (int i = 0; i < len; i++) {
+            for(int i = 0; i < len; i++){
                 char ch1 = word1.charAt(i);
                 char ch2 = word2.charAt(i);
-
-                if (ch1 != ch2) {
-                    if (map.get(ch1) > map.get(ch2)) return false;
+                
+                if(ch1 != ch2){
+                    if(arr[ch1 - 'a'] > arr[ch2 - 'a']) return false;
                     differenceFound = true;
                     break;
                 }
+                
             }
-
-            // Only check length if all chars matched and word1 is longer
-            if (!differenceFound && word1.length() > word2.length()) return false;
+            if(!differenceFound && word1.length() > word2.length()) return false;
         }
 
         return true;
