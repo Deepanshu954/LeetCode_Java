b84b1cc929cbb06144a6bad00a478f8da86113fb
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2025-11-23 13:41:40 +0530

    Time: 6 ms (66.17%), Space: 42.9 MB (43.97%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,30 +1,43 @@
+// class Solution {
+
+//     boolean isPalindrome(String str){
+//         int left = 0, right = str.length() - 1;
+
+//         while(left <= right){
+//             if(str.charAt(left) != str.charAt(right))return false;
+//             left++; right--;
+//         }
+//         return true;
+//     }
+
+//     public int countSubstrings(String s) {
+//         int n = s.length();
+//         int cnt = 0;
+
+//         for(int i = 0; i < n; i++) {
+//             for(int j = i + 1; j <= n; j++) {
+//                 String sub = s.substring(i,j);
+//                 if(isPalindrome(sub)) cnt++;
+//             }
+//         }
+//         return cnt;
+//     }
+// }
+
 class Solution {
-
-    boolean isPalindrome(String str){
-        int left = 0;
-        int right = str.length() - 1;
-        if(left == right) return true;
-
-        while(left <= right){
-            if(str.charAt(left) != str.charAt(right))
-                return false;
-
-            left++; right--;
-        }
-
-        return true;
-    }
-
     public int countSubstrings(String s) {
         int n = s.length();
         int cnt = 0;
 
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j <= n; j++) {
-                String sub = s.substring(i,j);
-                if(isPalindrome(sub)) cnt++;
+        for(int center = 0;  center < 2*n - 1; center++){
+            int left = center/2;
+            int right = left + center % 2;
+
+            while(left >= 0 && right < n && s.charAt(left) == s.charAt(right)){
+                cnt++; left--; right++;
             }
         }
+        
         return cnt;
     }
 }
