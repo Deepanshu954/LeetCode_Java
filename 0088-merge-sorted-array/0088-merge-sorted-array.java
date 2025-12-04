df16d08b912052e1c603f63d607558d946240e21
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2025-12-04 09:24:49 +0530

    Time: 0 ms (100%), Space: 43.9 MB (10.04%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,4 +1,31 @@
 class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int ptr1 = m - 1;
+        int ptr2 = n - 1;
+        int index = nums1.length - 1;
+
+        while(ptr1 >= 0 && ptr2 >= 0){
+            if(nums1[ptr1] > nums2[ptr2])
+            {
+                nums1[index--] = nums1[ptr1--];
+            }
+            else
+            {
+                nums1[index--] = nums2[ptr2--];
+            }
+        }
+
+        while(ptr2 >= 0){
+            nums1[index--] = nums2[ptr2--];
+        }
+        
+        return;
+    }
+}
+
+
+/*
+class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) 
     {
         int ptr1 = m - 1;
@@ -24,3 +51,4 @@ class Solution {
         }
     }
 }
+*/
