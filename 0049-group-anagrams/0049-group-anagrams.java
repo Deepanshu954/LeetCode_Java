ce9d06f80582b47cdf8991a439d70bafcf37509d
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2025-12-04 13:02:14 +0530

    Time: 6 ms (98.75%), Space: 49.5 MB (43.66%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,20 +1,53 @@
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+
+//         // Map<String, List<String>> map = new HashMap<>();
+
+//         // for(String s : strs)
+//         // {
+//         //     int[] freq = new int[26];
+//         //     for(char ch : s.toCharArray()) freq[ch - 'a']++;
+
+//         //     String key = Arrays.toString(freq);
+
+//         //     map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+
+//         // }
+
+//         // return new ArrayList<>(map.values());
+
+//         Map<String, List<String>> map = new HashMap<>();
+
+//         for(String s : strs){
+//             int[] freq = new int[26];
+//             for(char ch : s.toCharArray()) freq[ch - 'a']++;
+
+//             String key = Arrays.toString(freq);
+
+//             map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+//         }
+
+//         return new ArrayList<>(map.values());
+        
+//     }
+// }
+
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-
         Map<String, List<String>> map = new HashMap<>();
-
-        for(String s : strs)
-        {
-            int[] freq = new int[26];
-            for(char ch : s.toCharArray()) freq[ch - 'a']++;
-
-            String key = Arrays.toString(freq);
-
-            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
-
-        }
-
-        return new ArrayList<>(map.values());
         
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+            
+            map.get(sortedWord).add(word);
+        }
+        
+        return new ArrayList<>(map.values());
     }
 }
