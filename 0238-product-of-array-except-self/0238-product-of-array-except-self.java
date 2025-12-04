9f43e5cce63bc1116c1b06d5ce4d8c2d117461cf
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2025-12-04 09:16:15 +0530

    Time: 3 ms (23.83%), Space: 63.3 MB (51.89%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,22 +1,57 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] res = new int[n];
 
+        int[] suffix = new int[nums.length];int prod = 1;
 
-        int prefix = 1;
-        for(int i = 0; i < n; i++){
-            res[i] = prefix;
-            prefix *= nums[i];
-        }
-
-        int suffix = 1;
         for(int i = n - 1; i >= 0; i--){
-            res[i] *= suffix;
-            suffix *= nums[i];
+            prod *= nums[i];
+            suffix[i] = prod;
         }
 
-        return res;
-        
+        int[] prefix = new int[nums.length];
+        prod = 1;
+        for(int i = 0; i < n; i++){
+            prod *= nums[i];
+            prefix[i] = prod;
+        }
+
+        nums[0] = suffix[1];
+        nums[n-1] = prefix[n-2];
+
+        for(int i = 1; i < n - 1; i++){
+            nums[i] = prefix[i-1] * suffix[i+1]; 
+        }
+        return nums;
     }
 }
+
+/*
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+
+        int[] suffix = new int[nums.length];int prod = 1;
+        
+        for(int i = n - 1; i >= 0; i--){
+            prod *= nums[i];
+            suffix[i] = prod;
+        }
+
+        int[] prefix = new int[nums.length];
+        prod = 1;
+        for(int i = 0; i < n; i++){
+            prod *= nums[i];
+            prefix[i] = prod;
+        }
+
+        nums[0] = suffix[1];
+        nums[n-1] = prefix[n-2];
+
+        for(int i = 1; i < n - 1; i++){
+            nums[i] = prefix[i-1] * suffix[i+1]; 
+        }
+        return nums;
+    }
+}
+*/
