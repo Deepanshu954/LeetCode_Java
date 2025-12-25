20399b438d0f37e12c022e2dac1110d8be9ead6b
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2025-12-25 14:35:51 +0530

    Time: 20 ms (95.43%), Space: 81 MB (48.2%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,18 +1,24 @@
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//         HashSet<Integer> set = new HashSet<>();
+
+//         for(int i = 0; i < k; i++) set.add(nums[i]);
+//         for(int i = k; i < nums.length; i++){
+//             if(!set.add(nums[i])) return true;
+//             set.remove(nums[i-k]);
+//         }
+//         return false;
+//     }
+// }
+
+
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        int n = nums.length;
-        Set<Integer> set = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
 
-        for(int i = 0; i < n; i++){
-            if(set.contains(nums[i])){
-                return true;
-            }
-
-            set.add(nums[i]);
-
-            if(set.size() > k){
-                set.remove(nums[i-k]);
-            }
+        for(int i = 0; i < nums.length; i++){
+            if(!set.add(nums[i])) return true;
+            if(i - k >= 0) set.remove(nums[i-k]);
         }
         return false;
     }
