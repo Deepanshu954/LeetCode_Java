5d8bf81ef71a4c8e93329857fb9c05d26e0a25e8
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2025-12-28 14:45:36 +0530

    Time: 15 ms (64.6%), Space: 43.5 MB (78.33%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,21 +1,47 @@
+// // Brute Force
+// class Solution {
+//     public String longestPalindrome(String s) {
+//         if(s.length() < 2) return s;
+//         String ans = "";
+
+//         for(int i = 0; i < s.length(); i++){
+//             for(int j = i; j < s.length(); j++){
+//                 String sub = s.substring(i,j+1);
+//                 if(isPalindrome(sub) && ans.length() < sub.length()) ans = sub;
+//             }
+//         }
+//         return ans;
+//     }
+
+//     public static boolean isPalindrome(String s){
+//         int left = 0, right = s.length() - 1;
+
+//         while(left < right) if(s.charAt(left++) != s.charAt(right--)) return false;
+//         return true;
+//     }
+// }
+
 class Solution {
     public String longestPalindrome(String s) {
-        if(s.length() < 2) return s;
-        String ans = "";
+        int n = s.length();
+        int start = 0;
+        int maxLen = 1;
 
-        for(int i = 0; i < s.length(); i++){
-            for(int j = i; j < s.length(); j++){
-                String sub = s.substring(i,j+1);
-                if(isPalindrome(sub) && ans.length() < sub.length()) ans = sub;
+        for(int center = 0; center < 2*n -1; center++){
+            int left = center/2;
+            int right = left + center % 2;
+
+            while(left >= 0 && right < n && s.charAt(left) == s.charAt(right)){
+                int len = right - left + 1;
+
+                if(len > maxLen){
+                    maxLen = len;
+                    start = left;
+                }
+                left--;
+                right++;
             }
         }
-        return ans;
-    }
-
-    public static boolean isPalindrome(String s){
-        int left = 0, right = s.length() - 1;
-
-        while(left < right) if(s.charAt(left++) != s.charAt(right--)) return false;
-        return true;
+        return s.substring(start, start + maxLen);
     }
 }
