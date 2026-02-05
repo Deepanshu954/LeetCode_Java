755e81f76ae7865077d0609df9706bea32a39a94
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-02-05 10:19:44 +0530

    Time: 3 ms (97.49%), Space: 77.5 MB (24.03%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,18 +1,55 @@
 class Solution {
     public int maxArea(int[] height) {
+        int max = -1;
+        int min = -1;
+
         int left = 0;
         int right = height.length - 1;
 
-        int max = 0;
+        while(left < right) {
+            int localMax = 0;
 
-        while(left < right){
-            int ans = (right - left) * Math.min(height[left], height[right]);
+            if(height[left] <= height[right]) {
+                min = height[left++];
+            } else {
+                min = height[right--];
+            }
 
-            max = Math.max(max, ans);
+            localMax = min * (right - left + 1);
 
-            if(height[left] <= height[right]) left++;
-            else right--;
+            if(max < localMax) max = localMax;
         }
+
         return max;
     }
 }
+
+/*
+Brute Force
+
+class Solution {
+    public int maxArea(int[] height) {
+        
+        int max = -1;
+        int min = 0;
+
+        for(int i = 0; i < height.length; i++) {
+
+            int localMax = 0;
+
+            for(int j = i + 1; j < height.length; j++) {
+
+                if(height[i] < height[j]) min = height[i];
+                else min = height[j];
+
+                localMax = min * (j - i);
+
+                if(localMax > max) max = localMax;
+            }
+        }
+
+        return max;
+    }
+}
+
+*/
