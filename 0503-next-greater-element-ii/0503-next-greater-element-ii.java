b67f4aa6587fa81a4e6503c6605def17365cefee
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-02-09 08:53:15 +0530

    Time: 5 ms (94.04%), Space: 48.8 MB (29.84%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,27 +1,25 @@
-import java.util.*;
-
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
         Arrays.fill(res, -1);
 
-        Deque<Integer> dq = new ArrayDeque<>(); // store indices
+        Deque<Integer> dq = new ArrayDeque<>();
 
-        for (int i = 0; i < 2 * n; i++) {
+        for (int i = 2 * n - 1; i >= 0; i--) {
             int idx = i % n;
 
-            // resolve next greater
-            while (!dq.isEmpty() && nums[dq.peekFirst()] < nums[idx]) {
-                res[dq.removeFirst()] = nums[idx];
+            while (!dq.isEmpty() && dq.peekFirst() <= nums[idx]) {
+                dq.removeFirst();
             }
 
-            // push index only in first round
             if (i < n) {
-                dq.addFirst(idx);
+                res[idx] = dq.isEmpty() ? -1 : dq.peekFirst();
             }
-        }
 
+            dq.addFirst(nums[idx]);
+        }
+        
         return res;
     }
 }
