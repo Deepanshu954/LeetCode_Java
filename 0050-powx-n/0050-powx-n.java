b7a025182ecfa9b19c57d3f7898d8c5b726be9e3
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-02-16 12:36:51 +0530

    Time: 0 ms (100%), Space: 48.4 MB (21.63%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -11,13 +11,12 @@ class Solution {
     }
 
     private double fastPow(double x, long n) {
-        if (n == 0) return 1;
+        if(n == 0) return 1;
 
-        double half = fastPow(x, n / 2);
+        double half = fastPow(x, n/2);
 
-        if (n % 2 == 0)
-            return half * half;
-        else
-            return half * half * x;
+        if(n % 2 == 0) return half * half;
+        else return half * half * x;
+
     }
 }
