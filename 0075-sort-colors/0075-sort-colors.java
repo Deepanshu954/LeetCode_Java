de1f54250ac4e498240360c0ce3b16e70acafdbd
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-03-03 10:22:10 +0530

    Time: 0 ms (100%), Space: 43.7 MB (41.99%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,23 +1,49 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int a = 0, b = 0, c = 0;
+        int cnt0 = 0;
+        int cnt1 = 0;
+        int cnt2 = 0;
 
-        for(int num : nums) {
-            if(num == 0) a++;
-            else if(num == 1) b++;
-            else if(num == 2) c++;
+        for(int num : nums)
+        {
+            if(num == 0) cnt0++;
+            else if(num == 1) cnt1++;
+            else cnt2++;
         }
 
-        for(int i = 0; i < a; i++) {
-            nums[i] = 0;
-        }
+        int index = 0;
 
-        for(int i = 0; i < b; i++) {
-            nums[a + i] = 1;
-        }
-
-        for(int i = 0; i < c; i++) {
-            nums[a+b+i] = 2;
-        }
+        while(cnt0 > 0) {nums[index++] = 0; cnt0--;}
+        while(cnt1 > 0) {nums[index++] = 1; cnt1--;}
+        while(cnt2 > 0) {nums[index++] = 2; cnt2--;}
+        
+        return;
     }
 }
+
+
+
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         int low = 0, mid = 0, high = nums.length - 1;
+
+//         while (mid <= high) {
+//             if (nums[mid] == 0) {
+//                 swap(nums, low, mid);
+//                 low++;
+//                 mid++;
+//             } else if (nums[mid] == 1) {
+//                 mid++;
+//             } else {
+//                 swap(nums, mid, high);
+//                 high--;
+//             }
+//         }
+//     }
+
+//     private void swap(int[] nums, int i, int j) {
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+//     }
+// }
