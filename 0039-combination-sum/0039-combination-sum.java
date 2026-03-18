1095668299bc6df170d22f65d084f6a5c362a10b
Deepanshu Chauhan <137024426+Deepanshu954@users.noreply.github.com>
2026-03-18 15:53:05 +0530

    Time: 2 ms (95.5%), Space: 45.9 MB (29.74%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,26 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        BackTracking(0, candidates, target, res, new ArrayList<>());
-        return res;
+        List<List<Integer>> result = new ArrayList<>();
+        helper(candidates, 0, new ArrayList<>(), target, result);
+        return result;
     }
 
-    public static void BackTracking(int i, int[] arr, int target,
-                                    List<List<Integer>> list,
-                                    List<Integer> curr) {
-        if (i == arr.length) {
-            if (target == 0) {
-                list.add(new ArrayList<>(curr));
-            }
+    private void helper(int[] nums, int index, List<Integer> curr, int target, List<List<Integer>> result) {
+
+        if(index == nums.length) {
+            if(target == 0) result.add(new ArrayList<>(curr));
             return;
         }
 
-        if (arr[i] <= target) {
-            curr.add(arr[i]);
-            BackTracking(i, arr, target - arr[i], list, curr);
+        if(nums[index] <= target) {
+            curr.add(nums[index]);
+            helper(nums, index, curr, target - nums[index], result); 
             curr.remove(curr.size() - 1);
         }
 
-        BackTracking(i + 1, arr, target, list, curr);
+        helper(nums, index + 1, curr, target, result); 
+
     }
 }
